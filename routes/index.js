88b4cc1d0e6b3ff84f57d30923ff57f9376824e2
lvshihao <296877137@qq.com>
2017-12-23 15:48:29 +0800

    buggy

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,7 @@ router.post("/api/index/getarticlebytitle", async ctx => {
     let title = ctx.request.body.title;
     let article = await Article.getArticle({query: {title: title}});
     if (!article.length < 1) {
-        return ctx.render("index");
+        return await ctx.render("index");
     }
     ctx.response.type = 'application/json';
     ctx.response.body = {article: article};
