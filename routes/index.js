7af8591f1b416e497b32b0b6e4be616ffcecf114
lvshihao <296877137@qq.com>
2017-12-23 15:42:22 +0800

    buggy

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require("koa-router")();
 const Article = require("../module/article.js");
 
-// router.get("*",async ctx=>{
-//     await ctx.render("index");
-// });
+router.get("*",async ctx=>{
+    await ctx.render("index");
+});
 
 router.post("/api/index/getarticles", async (ctx,next) => {
     try {
