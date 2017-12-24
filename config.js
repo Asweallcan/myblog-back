b347fa950ec2c7ee9316ee3757f1caaaaa4ff841
lvshihao <296877137@qq.com>
2017-12-24 12:34:07 +0800

    buggy

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const produciton = true;
 
 module.exports = {
-    imagePath : __dirname+"/public/images",
+    imagePath : "/public/images",
     urlPath : produciton ? "http://111.230.230.75":"http://127.0.0.1:8088"
 };
