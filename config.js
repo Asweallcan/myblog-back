78b2a5da6e55f5d4bf107a9b212cfb0253269b47
lvshihao <296877137@qq.com>
2017-12-27 15:03:24 +0800

    修改接口地址

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const produciton = true;
 
 module.exports = {
     imagePath : "./public/images",
-    urlPath : produciton ? "http://lvshihaonb.cn":"http://127.0.0.1:8088"
+    urlPath : produciton ? "http://blog.lvshihaonb.cn":"http://127.0.0.1:8088"
 };
