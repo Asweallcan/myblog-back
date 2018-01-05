6930ed5299d127e435a2378b4cbc7e57cf049a23
lvshihao <296877137@qq.com>
2018-01-05 10:27:33 +0800

    cnm游民星空

--- controller/index.js
+++ controller/index.js
@@ -117,7 +117,7 @@ function getBizhi() {
                     let $element = $(element);
                     items.push({
                         title: $element.find(".con .tit").text(),
-                        link: $element.find(".img a").attr("href"),
+                        link: "http://www.gamersky.com/"+$element.find(".img a").attr("href"),
                         time: $element.find(".con .tme2 .time").text()
                     });
                 });
