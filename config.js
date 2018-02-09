32ea0b351b2b5758f93bd4f1bbec1ff5e212283b
lvshihao <296877137@qq.com>
2018-02-09 17:07:17 +0800

    bug

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
     backgroundSamllImagePath: "./public/backgrounds/small",
     movieImagePath: "./public/movies",
     urlPath: produciton
-        ? "http://localhost:8088"
-        : "http://server.lvshihaonb.cn"
+        ? "http://server.lvshihaonb.cn"
+        : "http://localhost:8088"
 };
