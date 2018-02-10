64a46ce032ff70af28a7be0780150846550b8b34
lvshihao <296877137@qq.com>
2018-02-10 09:29:58 +0800

    buy

--- routes/article.js
+++ routes/article.js
@@ -2,6 +2,6 @@ const ArticleController = require("../controller/article.js");
 const router = require("koa-router")();
 router.post("/api/article/uploadimage", ArticleController.uploader, ArticleController.uploadImage,ArticleController.uploadImageNext);
 router.post("/api/article/save", ArticleController.save);
-router.post("/api/article/getarticles", ArticleController.getArticles,ArticleController.getArticlesNext);
+router.post("/api/article/getarticles", ArticleController.getArticles);
 router.post("/api/article/delete", ArticleController.delete);
 module.exports = router;
